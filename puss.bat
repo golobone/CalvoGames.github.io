1d91b8992be7b029b3308696da4dadbf49948bee
golobone <keray.mendez@gmail.com>
2025-12-30 21:24:47 +0100

    prueba 2

---
+++ puss.bat
@@ -0,0 +1,41 @@
+@echo off
+title Panel de Control - Golo
+setlocal
+
+:: Tu ruta fija
+set "RUTA=C:\Users\Yeray Mendez\Desktop\CalvoGames\CalvoGames"
+
+:: Cambiar a la carpeta del proyecto
+cd /d "%RUTA%"
+
+echo ===========================================
+echo            SISTEMA DE SUBIDA GOLO
+echo ===========================================
+echo.
+
+:: 1. Bajar actualizaciones de la nube
+echo [ESTADO] Revisando si hay cambios en GitHub...
+git pull origin main --quiet
+
+echo.
+:: 2. Añadir archivos
+echo [INFO] Preparando archivos para subir...
+git add .
+
+echo.
+:: 3. Mensaje personalizado
+echo Escribe que has mejorado en el juego:
+set /p msg="> "
+
+:: 4. Commit y Push
+echo.
+echo [PROCESO] Guardando y enviando...
+git commit -m "%msg%" --quiet
+git push origin main --quiet
+
+echo.
+echo ===========================================
+echo       ¡TODO LISTO! CAMBIOS SUBIDOS
+echo ===========================================
+echo.
+pause
